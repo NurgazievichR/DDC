7f24216dca7a4ba6bb86bd4dc6b8c23e46afd0aa
NurgazievichR <zalalidinovroma@gmail.com>
2025-05-17 04:26:08 +0600

    registration

--- app/views.py
+++ app/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
-@login_required()
+@login_required
 def index(request):
     return render(request, 'app/index.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index') 
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
